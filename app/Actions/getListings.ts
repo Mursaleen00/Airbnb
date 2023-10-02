1cdb4fe199063d2447e48c69bcac8ee0ea1e848f
Mursaleen00 <playessport@gmail.com>
2023-10-02 15:30:48 +0500

    prisma prob

--- app/Actions/getListings.ts
+++ app/Actions/getListings.ts
@@ -5,7 +5,7 @@ export default async function getListing() {
     try {
         const listings = await prisma.listing.findMany({
             orderBy: {
-                createdAt: 'desc'
+                creatAt: 'desc'
             }
         })
 
